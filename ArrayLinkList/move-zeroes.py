875df770bb4b59928ee009f4b8caeb047ba0c2b1
niwang66 <37093190+niwang66@users.noreply.github.com>
2023-04-13 08:49:05 +0800

    Create move-zeroes.py

---
+++ ArrayLinkList/move-zeroes.py
@@ -0,0 +1,17 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        i,j=0,0
+        l = len(nums)
+        while i < l:
+            if(nums[i]==0):
+                None
+            else:
+                nums[j] = nums[i]
+                j+=1
+            i+=1
+        while j<l:
+            nums[j]=0
+            j +=1
+        """
+        Do not return anything, modify nums in-place instead.
+        """
